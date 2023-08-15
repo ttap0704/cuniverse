798bb68ec6a1d4b9707af165b169095fdc4814da
LeeDaeGyu <ttap0704@naver.com>
2023-08-15 15:46:26 +0900

    pm2 설정 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: 'cuniverse',
-      cwd: '/home/ubuntu/cuniverse',
-      script: '/home/ubuntu/cuniverse/node_modules/next/dist/bin/next',
-      args: 'start',
-      exec_mode: 'cluster',
+      name: "cuniverse",
+      cwd: "/home/ubuntu/cuniverse",
+      script: "yarn",
+      args: ["start"],
+      exec_mode: "cluster",
       instances: 0,
       autorestart: true,
       listen_timeout: 50000,
