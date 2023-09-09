d960193c0f3802d31241d29c03e67756f6633e84
LeeDaeGyu <ttap0704@naver.com>
2023-09-09 14:48:32 +0900

    dev: assets UI 오류 수정 & input file accept 추가가

--- src/components/inputs/InputImage.tsx
+++ src/components/inputs/InputImage.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { setModalAlertAtom } from "@/store/modalAlert";
+import { useSetAtom } from "jotai";
 import React from "react";
 
 // Image Upload를 위한 Input Component
@@ -12,8 +14,35 @@ interface InputImageProps {
 function InputImage(props: InputImageProps) {
   const id = props.id;
   const onChange = props.onChange;
+  const setModalAlert = useSetAtom(setModalAlertAtom);
 
-  return <input type="file" id={id} name={id} onChange={onChange} />;
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (files && files[0]) {
+      const file = files[0];
+      const limit = 20000000; // 10MB;
+
+      if (limit < file.size) {
+        setModalAlert({
+          type: "error",
+          text: "이미지는 최대 20MB까지 업로드가 가능합니다.",
+          open: true,
+        });
+        return;
+      }
+    }
+    onChange(e);
+  };
+
+  return (
+    <input
+      type="file"
+      id={id}
+      name={id}
+      onChange={handleFile}
+      accept="image/*"
+    />
+  );
 }
 
 export default InputImage;
